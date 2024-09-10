fbd9217a26a64fdf550b730de0c0aa1fd6666f48
jimjasonp <132594623+jimjasonp@users.noreply.github.com>
2024-09-10 11:25:26 +0300

    Add files via upload
    updated inital programms so that they are parametric

--- esyd_diakribwsh.py
+++ esyd_diakribwsh.py
@@ -5,25 +5,45 @@ einai sumfwnh me thn pistopoihsh
 To programma kanei extract ena csv to opoio periexei tiw metrhseis pou grafontai sto 
 pistopoihtiko diakrivwshs
 '''
+from pathlib import Path
 import pandas as pd
-
+import numpy as np
 from periballontikes_sunthikes import therm_anaf,therm_organ,ygr_anaf,ygr_organ,piesi_organ,piesi_anaf
 
 
-def sfalma(anaf,organ):
-    sfalma = organ-anaf
-    print(sfalma)
+def sfalma(name,anaf,organ):
+    sfalma_list = []
+    i=0
+    while i<len(organ):
+        sfalma = organ[i]-anaf[i]
+        sfalma_list.append(sfalma)
+        i = i+1
+    df = pd.DataFrame(sfalma_list, columns=[name])
+    filepath = Path(f'results/{name}.csv') 
+    df.to_csv(filepath)
+    
 
-def shmeia_allaghs(organ):
+def shmeia_allaghs(name,organ):
     i = 0 
+    shmeia_allaghs_list = []
     while i<len(organ)-1:
         shmeia_allaghs = (organ[i]+organ[i+1])/2
+        shmeia_allaghs_list.append(shmeia_allaghs)
         i = i+1
+    df = pd.DataFrame(shmeia_allaghs_list, columns=[name])
+    filepath = Path(f'results/{name}.csv') 
+    df.to_csv(filepath)
+
+sfalma('sfalma_thermokrasias',therm_anaf,therm_organ)
+sfalma('sfalma_ygrasias',ygr_anaf,ygr_organ)
+sfalma('sfalma_piesis',piesi_anaf,piesi_organ)
+shmeia_allaghs('shmeia_allaghs_thermokrasias',therm_organ)
+shmeia_allaghs('shmeia_allaghs_ygrasias',ygr_organ)
+shmeia_allaghs('shmeia_allaghs_piesis',piesi_organ)
 
 
-sfalma_therm = sfalma(therm_anaf,therm_organ)
 
 
-shmeia_allaghs_therm = shmeia_allaghs(therm_organ)
 
-### thelo na kano extract ta apotelesmata se csv
+
+### thelo na bazei ta csv se ksexwirsto fakelo "results"
--- periballontikes_sunthikes.py
+++ periballontikes_sunthikes.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-list_metrhseis = pd.read_csv('metrhseis.csv')
+list_metrhseis = pd.read_csv('metrhseis_periballontos.csv')
 list_metrhseis = list_metrhseis.dropna()
 column_names = list_metrhseis.columns
 therm_anaf = list_metrhseis['Thermokrasia_anaforas']
@@ -10,5 +10,5 @@ therm_organ = list_metrhseis['Thermokrasia_organou']
 ygr_anaf = list_metrhseis['Ygrasia_anaforas']
 ygr_organ = list_metrhseis['Ygrasia_organou']
 piesi_anaf = list_metrhseis['Barometriki_piesi_anaforas']
-piesi_organ = ['barometriki_piesi_organou']
+piesi_organ = list_metrhseis['barometriki_piesi_organou']
 
